70e5b7fa691524a80c66dc4b0c81b3de1ce46102
artem babakhaniants <artem@edadeal.ru>
2017-12-28 15:40:32 +0300

    Initial commit for Week 1 assignment

---
+++ Week1/KaratsubaMultiplication/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  KaratsubaMultiplication
+//
+//  Created by Artem Babakhanyants on 28.12.17.
+//  Copyright © 2017 Artem Babakhaniants. All rights reserved.
+//
+
+import Foundation
